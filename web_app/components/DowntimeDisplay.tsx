7cfb7b41a99c225421150d964d22b556df3a5afe
Matyáš Trnka <hello@matrn.dev>
2026-04-04 00:34:30 +0200

    add downtime display

--- web_app/components/DowntimeDisplay.tsx
+++ web_app/components/DowntimeDisplay.tsx
@@ -1,14 +1,17 @@
 import DowntimeSegment from "./DowntimeSegment"
 
-const segmentCount = 30;
 
-export default function DowntimeDisplay() {
+export default async function DowntimeDisplay() {
+    const data = await fetch("http://localhost:8000/alerts/route/L401/downtime/chunks?chunk_size_hours=24&chunk_count=30");
+    let chunks = await data.json();
+    chunks = chunks.reverse();
+    console.log(chunks)
     return (
         <div className="m-2 rounded-md border-2 border-lime-900 p-4">
             <div className="mb-2 text-sm font-medium text-lime-700">Last 30 days</div>
             <div className="flex items-stretch gap-1">
-                {Array.from({ length: segmentCount }, (_, i) => (
-                    <DowntimeSegment key={i} />
+                {Array.from({ length: chunks.length }, (_, i) => (
+                    <DowntimeSegment key={i} alerts={chunks[i].alerts} />
                 ))}
             </div>
         </div>
--- web_app/components/DowntimeSegment.tsx
+++ web_app/components/DowntimeSegment.tsx
@@ -1,5 +1,18 @@
-export default function DowntimeSegment() {
-    return (
-        <span className="flex-1 h-8 rounded-sm bg-lime-500"></span>
-    );
+interface Alert {
+  id: string;
+  cause: string;
+  effect: string;
+  descriptionText: {
+    translation: {
+      text: string;
+    }[];
+  };
+}
+
+export default function DowntimeSegment({ alerts }: { alerts: Array<Alert> }) {
+    if (alerts.length === 0) {
+        return <span className="flex-1 h-8 rounded-sm bg-lime-500"></span>;
+    } else {
+        return <span className="flex-1 h-8 rounded-sm bg-red-500"></span>;
+    }
 }
