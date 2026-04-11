f49ffb82393cbc566fb449376f81ca805d994a8e
Matyáš Trnka <hello@matrn.dev>
2026-04-11 20:50:01 +0200

    don't print alerts each run

--- api/main.py
+++ api/main.py
@@ -4,14 +4,14 @@ load_dotenv()
 
 import alerts
 import database
+import datetime
 
 current_alerts = alerts.get_current()
 
-print("Current alerts:")
 for entity in current_alerts:
     database.upsert_alert(entity)
-    print(entity)
+    #print(entity)
 
 database.end_orphaned() # needs proper testing
 
-print("\n\n\nAlerts updated successfully")
+print("\n\n\nAlerts updated successfully ", datetime.datetime.now())
