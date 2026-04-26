4373d0a8a7ad083521fe9c9852bd765a6001d685
Matyáš Trnka <hello@matrn.dev>
2026-04-26 14:25:25 +0200

    bugfixes

--- api/api.py
+++ api/api.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 import database
 import downtime
 import json
 import gtfs_static
 
 app = FastAPI()
+
+# origins = [
+#     "http://localhost",
+#     "http://localhost:3000",
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 POSSIBLE_EFFECTS = ["NO_SERVICE", "REDUCED_SERVICE", "SIGNIFICANT_DELAYS", "DETOUR", "ADDITIONAL_SERVICE", "MODIFIED_SERVICE", "OTHER_EFFECT", "UNKNOWN_EFFECT", "STOP_MOVED", "ACCESSIBILITY_ISSUE", "NO_EFFECT"]
 
@@ -30,7 +44,7 @@ async def get_alerts_for_route(route_id: str, last_days: float = None, filter_ef
                 raise HTTPException(status_code=400, detail=f"Invalid effect")
     
     data = database.get_alerts_for_route(route_id, last_days, important_only, filter_effects=picked_effects, timeframe_start=start, timeframe_end=end)
-    return json.loads(json.dumps(data, default=str)) if data else {"detail": "No relevant results found"}
+    return json.loads(json.dumps(data, default=str))
 
 @app.get("/alerts/route/{route_id}/downtime")
 async def get_downtime(route_id: str, last_days: float, filter_effects: str = None):
@@ -44,7 +58,7 @@ async def get_downtime(route_id: str, last_days: float, filter_effects: str = No
                 raise HTTPException(status_code=400, detail=f"Invalid effect")
 
     data = downtime.downtime_for_route(route_id, downtime_window_days=last_days, important_only=important_only, filter_effects=picked_effects)
-    return json.loads(json.dumps(data, default=str)) if data else {"detail": "No relevant results found"}
+    return json.loads(json.dumps(data, default=str)) 
 
 # todo: we should outsource picked effects checking to a different function since it's used in multiple places
 @app.get("/alerts/route/{route_id}/downtime/chunks")
@@ -61,7 +75,7 @@ async def get_downtime_chunks(route_id: str, chunk_size_hours: float, chunk_coun
 
     data = downtime.calculate_chunks(route_id, chunk_size_hours, chunk_count, important_only=important_only, filter_effects=picked_effects)
     if not data:
-        return {"detail": "No relevant results found"}
+        return {}
     
     return {
         "chunks": json.loads(json.dumps(data[0], default=str)),
@@ -71,4 +85,9 @@ async def get_downtime_chunks(route_id: str, chunk_size_hours: float, chunk_coun
 @app.get("/info/route/{route_id}")
 async def get_route_by_id(route_id: str):
     data = gtfs_static.get_route_by_id(route_id)
-    return data if data else {"detail": "No relevant results found"}
+    return data 
+
+@app.get("/info/all-routes")
+async def get_routes():
+    data = gtfs_static.get_routes()
+    return data 
--- api/gtfs_static.py
+++ api/gtfs_static.py
@@ -3,22 +3,45 @@ import partridge as prg
 path = "gtfs_examples/PID_GTFS.zip"
 feed = prg.load_feed(path)
 
+
 def get_route_by_id(route_id):
     route = feed.routes[feed.routes.route_id == route_id].to_dict(orient="records")[0]
     if not route:
         return None
 
-    routeToSend = {
-        "route_id": route["route_id"],
-        "agency_id": route["agency_id"],
-        "route_short_name": route["route_short_name"],
-        "route_long_name": route["route_long_name"],
-        "route_type": route["route_type"],
-        "route_url": route["route_url"],
-        "route_color": route["route_color"],
-        "route_text_color": route["route_text_color"],
-        "is_night": int(route["is_night"]) == 1,
-        "is_regional": int(route["is_regional"]) == 1,
-        "is_substitute_transport": int(route["is_substitute_transport"]) == 1
+    routeToSend = { 
+        "route_id": str(route["route_id"]),
+        "agency_id": str(route["agency_id"]),
+        "route_short_name": str(route["route_short_name"]),
+        "route_long_name": str(route["route_long_name"]),
+        "route_type": int(route["route_type"]),
+        "route_url": str(route["route_url"]),
+        "route_color": str(route["route_color"]),
+        "route_text_color": str(route["route_text_color"]),
+        "is_night": bool(int(route["is_night"])),
+        "is_regional": bool(int(route["is_regional"])),
+        "is_substitute_transport": bool(int(route["is_substitute_transport"])),
     }
     return routeToSend
+
+
+def get_routes():
+    routes = feed.routes.to_dict(orient="records")
+    routesToSend = []
+    for route in routes:
+        routesToSend.append(
+            {
+                "route_id": str(route["route_id"]),
+                "agency_id": str(route["agency_id"]),
+                "route_short_name": str(route["route_short_name"]),
+                "route_long_name": str(route["route_long_name"]),
+                "route_type": int(route["route_type"]),
+                "route_url": str(route["route_url"]),
+                "route_color": str(route["route_color"]),
+                "route_text_color": str(route["route_text_color"]),
+                "is_night": bool(int(route["is_night"])),
+                "is_regional": bool(int(route["is_regional"])),
+                "is_substitute_transport": bool(int(route["is_substitute_transport"])),
+            }
+        )
+    return routesToSend
