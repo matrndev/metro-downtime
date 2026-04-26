21da944fdd4e742a1cff9e28350ec43f25f931c2
Matyáš Trnka <hello@matrn.dev>
2026-04-26 21:42:52 +0200

    show active alert cards on landing page

--- web_app/app/page.tsx
+++ web_app/app/page.tsx
@@ -1,21 +1,29 @@
+import AlertCard from "@/components/AlertCard";
 import DowntimeDisplay from "@/components/DowntimeDisplay";
 import RouteSearchBox from "@/components/RouteSearchBox";
 import { Alert } from "@/types/apiResponses";
 
-async function getActiveAlerts(): Promise<Alert[]> {
+
+async function getAlerts(): Promise<Alert[]> {
   const res = await fetch("http://localhost:8000/alerts/route/*?last_days=1");
   return (await res.json()) as Alert[];
 }
 
+async function getActiveAlerts(): Promise<Alert[]> {
+  const res = await fetch("http://localhost:8000/alerts/all-active");
+  return (await res.json()) as Alert[];
+}
+
 export default async function Home() {
-  const activeAlerts: Alert[] = await getActiveAlerts();
+  const alerts: Alert[] = await getAlerts();
   const routes: string[] = [
     ...new Set(
-      activeAlerts.flatMap((alert: Alert) =>
+      alerts.flatMap((alert: Alert) =>
         alert.informedEntity.map((entity) => entity.routeId)
       )
     ),
   ];
+  const activeAlerts = await getActiveAlerts();
 
 
   return (
@@ -33,6 +41,11 @@ export default async function Home() {
             chunkSizeHours={0.5}
             route={routeId}
           />
+        ))}
+        <br /><br />
+        <h1 className="text-2xl font-bold mx-2">Currently active alerts:</h1>
+        {activeAlerts.map((alert) => (
+          <AlertCard key={alert.id} alert={alert} />
         ))}
       </div>
     </div>
